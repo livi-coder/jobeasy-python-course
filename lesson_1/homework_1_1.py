5d222399488ceedd542db9d41fb07381f48ddb0f
livi-coder <69655179+livi-coder@users.noreply.github.com>
2020-08-15 11:57:57 -0400

    Update homework_1_1.py

--- lesson_1/homework_1_1.py
+++ lesson_1/homework_1_1.py
@@ -2,28 +2,28 @@
 
 a = 10
 b = 15
-result_sum = None
+result_sum = 5
 
 
 # Find the difference between c and d. Save the expression to the result_diff variable
 
 c = 10
 d = 15
-result_diff = None
+result_diff = -5
 
 
 # Multiply e by f. Save the expression to the result_multiplication variable
 
 e = 10
 f = 15
-result_multiplication = None
+result_multiplication = 150
 
 
 # Divide g by h. Save the expression to the result_division variable
 
 g = 10
 h = 15
-result_division = None
+result_division = 0.66
 
 
 # Calculate the floored division result of i and j. Save the expression to the result_division_floored variable
@@ -37,17 +37,17 @@ result_division_floored = None
 
 k = 10
 l = 15
-result_division_remainder = None
+result_division_remainder = 0
 
 
 # Rise m to the power n. Save the expression to the result_power variable
 
 m = 5
 n = 2
-result_power = None
+result_power = 25
 
 
 # Convert an o variable to negative. Save the expression to the result_negative variable
 
 o = 100
-result_negative = None
+result_negative =-100
