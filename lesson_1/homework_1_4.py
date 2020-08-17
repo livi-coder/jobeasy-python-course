9df2243d649949cf5de9dc97834a4965b74a06f5
livi-coder <69655179+livi-coder@users.noreply.github.com>
2020-08-17 10:20:57 -0400

    Update homework_1_4.py

--- lesson_1/homework_1_4.py
+++ lesson_1/homework_1_4.py
@@ -1,28 +1,28 @@
 # Create three strings using three different methods. Save your result to result_string_1, result_string_2,
 # result_string_3 variables
 
-result_string_1 = None
-result_string_2 = None
-result_string_3 = None
+result_string_1 = "I 
+result_string_2 = "like"
+result_string_3 = "Chemistry and Physics"
 
 
 # Enter your first and  last name. Join them together with a space in
 # between. Save a result in a variable result_full_name and
 # save the length of the whole name in result_full_name_length variable.
 
-first_name = None
-last_name = None
-result_full_name = None
-result_full_name_length = None
+first_name = 'Livia'
+last_name = 'Lufi'
+result_full_name = 'LiviaLufi'
+result_full_name_length = 'LiviaLufi'
 
 
 # Enter the capital city of California State in lower case. Change the case to title case.
 # Save the result in result_ca_capital variable
 
-result_ca_capital = None
+result_ca_capital = 'sacramento'
 
 
 # Enter the name of our planet. Change the case to upper case. Save the result in
 # result_planet variable
 
-result_planet = None
+result_planet = 'Earth'
