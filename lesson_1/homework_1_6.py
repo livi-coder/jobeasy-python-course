f94cfa25b44cb178f9c7bdd6e857fab5e6f14652
livi-coder <69655179+livi-coder@users.noreply.github.com>
2020-08-17 11:13:00 -0400

    Update homework_1_6.py

--- lesson_1/homework_1_6.py
+++ lesson_1/homework_1_6.py
@@ -2,6 +2,6 @@
 # a string (e.g. '75') and convert it to Celsius.
 # !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
 
-# type your code here
-temperature_fahrenheit = None
-result_temperature = None
+# string_75
+temperature_fahrenheit = (32-165)*5/9
+result_temperature = -73.88
