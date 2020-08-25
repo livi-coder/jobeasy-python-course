28087ca0c4a217e633e158b84b197fa04422ead1
livi-coder <69655179+livi-coder@users.noreply.github.com>
2020-08-25 15:20:35 -0400

    Update homework_3_1.py

--- lesson_3/homework_3_1.py
+++ lesson_3/homework_3_1.py
@@ -3,8 +3,11 @@
 # Ex. 1
 # Enter your name, save it in name variable and save in result_1 variable your name repeated 3 times (use loops)
 
-name_1 = None
-result_1 = None
+name_1 = 'Livia'
+result_1 = 'Livia'
+
+for_Livia
+else: Livia
 
 # TODO: Here is your code
 
@@ -13,9 +16,12 @@ result_1 = None
 # Modify your previous program so that it will enter your name (save it in variable  name_2) and a number
 # (save in variable number) and then save in result_2 variable your name repeated as many times as number_1 is
 # (use loops)
-name_2 = None
-number_1 = None
-result_2 = None
+name_2 = 'Livia'
+number_1 = 7
+result_2 = 
+
+for_Livia is number 7
+else: Number 7
 
 # TODO: Here is your code
 
@@ -24,8 +30,11 @@ result_2 = None
 # Enter a random string, which includes only digits. Write code which find a sum of digits in this string and save it
 # into result_3 variable
 
-string_number_1 = None
-result_3 = None
+string_number_1 = 875
+result_3 = 
+
+for_string_number_1= 875
+else: string_number_1= 875
 
 # TODO: Here is your code
 
@@ -33,6 +42,9 @@ result_3 = None
 # Ex. 4
 # Create code which sums up all even numbers between 2 and 100 (include 100) and save it in result_4 variable
 
-result_4 = None
+result_4 = 
+
+for_number= 2*99
+else: number= 198
 
 # TODO: Here is your code
