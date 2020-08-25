4e100f5072ce48f6b084033e2ce688741e2c264e
livi-coder <69655179+livi-coder@users.noreply.github.com>
2020-08-25 15:28:36 -0400

    Update homework_3_2.py

--- lesson_3/homework_3_2.py
+++ lesson_3/homework_3_2.py
@@ -4,20 +4,26 @@
 # Write code, which will count how many times your character is included in your string.
 # Save result to result_1 variable
 
-string_1 = None
-char_1 = None
-result_1 = None
-
+string_1 = 22
+char_1 = 22
+result_1 = char_1[string_1] = 22
+for_char(string_1) =22
+else: string_1 = 22
 
 # Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
 # and save result in the result_2 variable.
 
-number_1 = None
+number_1 = 65
 result_2 = None
 
+for_number_1 = 65
+else: number_1 * 65 =30
 
 # Enter a random number and save it in variable number_2. Then create code which will return
 # a number with digits of number_1 in reverse order. Save it in result_3 variable
 
-number_2 = None
-result_3 = None
+number_2 = 59
+result_3 = 95
+
+while_reverse the number = 59
+else: the number reversed is 95
